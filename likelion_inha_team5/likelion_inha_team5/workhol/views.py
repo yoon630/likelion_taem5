77da62cd7741bb389ff31c9f7409867eccc8ed42
YeomJongSeop <bhs3435@naver.com>
2024-06-30 15:37:52 +0900

    Initial commit with current state

---
+++ likelion_inha_team5/likelion_inha_team5/workhol/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+import json
+from django.http import JsonResponse
+from .models import *
+from django.shortcuts import get_object_or_404
+
+# Create your views here.
